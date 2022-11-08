ef39a08ab5077bf05ad1c9f835210b9ef13bf5f0
Daniel Roe <daniel@roe.dev>
2022-11-08 12:02:33 +0100

    chore: init

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    {{ $route }}
+  </div>
+</template>
+
+<script setup>
+const route = useRoute()
+const myCookie = useCookie('myCookie')
+const value = useSomething()
+const { data: name } = await useFetch('/api/test')
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+  // layout: ''
+  // middleware: '',
+})
+</script>
+
+<template>
+  <div>
+    {{ $auth.login }}
+  </div>
+</template>
